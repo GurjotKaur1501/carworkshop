a3a6b17f9b61d5bec25c3b5ee483884869b877a4
GurjotKaur1501 <jotkau0115@gmail.com>
2025-11-25 09:41:50 +0100

    Adding dtos classes

--- src/main/java/se/yrgo/carworkshop/dto/CustomerDto.java
+++ src/main/java/se/yrgo/carworkshop/dto/CustomerDto.java
@@ -1,4 +1,36 @@
 package se.yrgo.carworkshop.dto;
 
+import java.util.List;
+
 public class CustomerDto {
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private List<VehicleDto> vehicles;
+
+    public CustomerDto() {
+    }
+
+    public CustomerDto(Long id, String name, String phoneNumber, List<VehicleDto> vehicles) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.vehicles = vehicles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public List<VehicleDto> getVehicles() {
+        return vehicles;
+    }
 }
--- src/main/java/se/yrgo/carworkshop/dto/VehicleDto.java
+++ src/main/java/se/yrgo/carworkshop/dto/VehicleDto.java
@@ -1,4 +1,46 @@
 package se.yrgo.carworkshop.dto;
 
 public class VehicleDto {
+    private Long id;
+    private String registrationNumber;
+    private String brand;
+    private String model;
+    private Integer productionYear;
+    private Long customerId;
+
+    public VehicleDto() {
+    }
+
+    public VehicleDto(Long id, String registrationNumber, String brand, String model, Integer productionYear, Long customerId) {
+        this.id = id;
+        this.registrationNumber = registrationNumber;
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.customerId = customerId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getProductionYear() {
+        return productionYear;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
 }
