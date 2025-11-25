8cb0684602f5bdebf321a9bb43bc9dd91f1a3a18
GurjotKaur1501 <jotkau0115@gmail.com>
2025-11-25 09:45:01 +0100

    updating propoerties and sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers(id,name,phone_number) VALUES (1,'Anna','0123456789');
+INSERT INTO vehicles(id,registration_number,brand,model,production_year,customer_id) VALUES (1,'ABC123','Volvo','V70',2008,1);
